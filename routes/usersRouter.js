4f546de530b877b76157aaeb21edb5e888750f59
Etativel <farhanmaulana4611@gmail.com>
2024-12-22 16:17:02 +0800

    feat: get username from database

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,6 +1,7 @@
 // routes/usersRouter.js
 const { Router } = require("express");
 const usersController = require("../controllers/usersController");
+const dbController = require("../controllers/dbController");
 const usersRouter = Router();
 
 usersRouter.get("/", usersController.usersListGet);
@@ -10,5 +11,5 @@ usersRouter.get("/search", usersController.usersSearch);
 usersRouter.get("/:id/update", usersController.usersUpdateGet);
 usersRouter.post("/:id/update", usersController.usersUpdatePost);
 usersRouter.post("/:id/delete", usersController.userDeletePost);
-
+usersRouter.get("/db/user", dbController.getUsernames);
 module.exports = usersRouter;
