5169da8d7bbced63c2f09c312d6fbdd01794de30
Etativel <farhanmaulana4611@gmail.com>
2024-12-23 21:03:44 +0800

    feat: add env file

--- db/pool.js
+++ db/pool.js
@@ -1,9 +1,10 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 module.exports = new Pool({
-  host: "localhost", // or wherever the db is hosted
-  user: "postgres",
-  database: "top_users",
-  password: "admin123081",
+  host: process.env.HOST, // or wherever the db is hosted
+  user: process.env.USER,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
   port: 5432, // The default port
 });
